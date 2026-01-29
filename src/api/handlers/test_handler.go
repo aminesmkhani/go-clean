65d876e226e72b658d59ce941d4de5b4849870bb
Amin <amin.esmkhani75@gmail.com>
2026-01-29 12:16:28 +0330

    work with all binder type

--- src/api/handlers/test_handler.go
+++ src/api/handlers/test_handler.go
@@ -6,6 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
+type headers struct{
+	UserId string
+	Browser string
+}
+
+type personData struct{
+	FirstName string
+	LastName string
+}
 type TestHandler struct{
 
 }
@@ -56,5 +66,79 @@ func(h *TestHandler) AddUser(c *gin.Context){
 	})
 }
 
+
+func (h *TestHandler) HeaderBinder1(c *gin.Context){
+
+	userID := c.GetHeader("UserId")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"userId": userID,
+	})
+
+}
+
+func (h *TestHandler) HeaderBinder2(c *gin.Context){
+	header := headers{}
+	c.BindHeader(&header)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder2",
+		"header": header,
+	})
+
+}
+func (h *TestHandler) QueryBinder1(c *gin.Context){
+	id := c.Query("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBinder 1",
+		"id": id,
+		"name": name,
+	})
+
+}
+func (h *TestHandler) QueryBinder2(c *gin.Context){
+	ids := c.QueryArray("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBinder 1",
+		"id": ids,
+		"name": name,
+	})
+
+}
+
+func (h *TestHandler) UriBinder(c *gin.Context){
+
+	id := c.Param("id")
+	name := c.Param("name")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "UriBinder",
+		"id": id,
+		"name": name,
+	})
+
+}
+func (h *TestHandler) BinderBody(c *gin.Context){
+
+	p := personData{}
+	c.ShouldBindJSON(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "BinderBody",
+		"data" : p,
+	})
+
+}
+func (h *TestHandler) BinderBody1(c *gin.Context){
+
+	p := personData{}
+	c.ShouldBindJSON(&p)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "BinderBody",
+		"data" : p,
+	})
+
+}
 
 
--- src/api/routers/test_router.go
+++ src/api/routers/test_router.go
@@ -16,4 +16,18 @@ func TestRouter(r *gin.RouterGroup){
 	r.GET("/user/get-user-by-username/:username",h.UserByUserName)
 	r.GET("/user/:id/accounts",h.Accounts)
 	r.POST("/add-user",h.AddUser)
+
+	r.GET("/header1",h.HeaderBinder1)
+	r.GET("/header2",h.HeaderBinder2)
+
+
+	r.GET("/binder/query1",h.QueryBinder1)
+	r.GET("/binder/query2",h.QueryBinder2)
+	r.GET("/binder/uri/:id/:name",h.UriBinder)
+
+	r.POST("/binder/body",h.BinderBody)
+	
+
 }
+
+
