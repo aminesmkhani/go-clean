f1a73ac23dfd1dff2d555e56aec2f5f9d24756b3
Amin <amin.esmkhani75@gmail.com>
2026-02-15 10:49:55 +0330

    add logger for persian validation

--- src/common/persian.go
+++ src/common/persian.go
@@ -1,16 +1,20 @@
 package common
 
 import (
-	"log"
 	"regexp"
+
+	"github.com/aminesmkhani/go-clean/config"
+	"github.com/aminesmkhani/go-clean/pkg/logging"
 )
+
+var logger = logging.NewLogger(config.GetConfig()) 
 
 const iranianMobileNumberPattern string = `^09(1[0-9]|2[0-2]|3[0-9]|9[0-9])[0-9]{7}$`
 
 func IranianMobileNumberValidate(mobileNumber string) bool {
 	res, err := regexp.MatchString(iranianMobileNumberPattern, mobileNumber)
 	if err != nil {
-		log.Print(err.Error())
+		logger.Error(logging.Validation, logging.MobileValidation, err.Error(),nil)
 	}
 	return res
 }
