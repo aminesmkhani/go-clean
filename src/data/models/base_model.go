18dc793846ddee91508a3b0b63cfa4783edd4fda
Amin <amin.esmkhani75@gmail.com>
2026-02-17 21:00:43 +0330

    create Base mor all models

--- src/data/models/base_model.go
+++ src/data/models/base_model.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type BaseModel struct {
+	ID        uint   `gorm:"primaryKey" json:"id"`
+	CreatedAt time.Time `gorm:"type:TIMESTAMP with time zone; not null"`
+	ModifiedAt sql.NullTime `gorm:"type:TIMESTAMP with time zone; null"`
+	DeletedAt sql.NullTime `gorm:"type:TIMESTAMP with time zone; null"`
+
+	CreatedBy uint `gorm:"not null"`
+	ModifiedBy int `gorm:"null"`
+	DeletedBy int `gorm:"null"`
+}
