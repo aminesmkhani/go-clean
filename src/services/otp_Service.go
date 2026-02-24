06fdcddb61897030e69b779945c1446c0dd7eeec
Amin <amin.esmkhani75@gmail.com>
2026-02-24 09:58:02 +0330

    create core for dependency injection otp service and base class for new instance

--- src/services/otp_Service.go
+++ src/services/otp_Service.go
@@ -1,2 +1,25 @@
 package services
 
+import (
+	"github.com/aminesmkhani/go-clean/config"
+	"github.com/aminesmkhani/go-clean/data/cache"
+	"github.com/aminesmkhani/go-clean/pkg/logging"
+	"github.com/redis/go-redis/v9"
+)
+
+type OtpService struct {
+	logger logging.Logger
+	cfg    *config.Config
+	redis  *redis.Client
+}
+
+
+func NewOtpService(cfg *config.Config) *OtpService {
+	logger := logging.NewLogger(cfg)
+	redis := cache.GetRedis()
+	return &OtpService{
+		logger: logger,
+		cfg:    cfg,
+		redis:  redis,
+	}
+}
