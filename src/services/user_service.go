3dda369dffeda712ed999a97e6ada88eee881e42
Amin <amin.esmkhani75@gmail.com>
2026-02-25 06:00:15 +0330

    create base function for user service

---
+++ src/services/user_service.go
@@ -0,0 +1,30 @@
+package services
+
+import (
+	"github.com/aminesmkhani/go-clean/config"
+	"github.com/aminesmkhani/go-clean/data/db"
+	"github.com/aminesmkhani/go-clean/pkg/logging"
+	"gorm.io/gorm"
+)
+
+
+
+type UserService struct {
+	logger logging.Logger
+	cfg 	*config.Config
+	otpService *OtpService
+	database   *gorm.DB
+}
+
+
+
+func NewUserService(cfg *config.Config) *UserService {
+	database := db.GetDb()
+	logger := logging.NewLogger(cfg)
+	return &UserService{
+		logger: logger,
+		cfg: cfg,
+		database: database,
+		otpService: NewOtpService(cfg),
+	}
+}
